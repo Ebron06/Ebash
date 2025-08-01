2e54ee646e7c363320322c54e8fdbcd45a018388
Ebron06 <chrisebron7@icloud.com>
2025-08-01 09:09:48 -0400

    username and hostname created

--- main.c
+++ main.c
@@ -16,17 +16,17 @@ char* get_username() {
         //pw can fail if the user doesn't exist or due to sys errors
         perror("Error retrieving password info for current user\n");
     }
-    return pw;
+    return pw->pw_name;
 };
 
-char* get_hostname(buf, sizeof(buf)) {
-
+char* get_hostname(char* buf, size_t size) {
+    
     if(gethostname(buf, sizeof(buf)) == 0) {
         return buf;
     } else{
         perror("Error retrieving hostname\n");
     }
-    return buf;
+    return NULL;
 };
 
 
@@ -34,10 +34,12 @@ int main()
 {
 	int close = 1;
     char hostname[HOST_NAME_MAX + 1];  //Buffer to store hostname, +1 for null terminator
+    char* host = get_hostname(hostname, sizeof(hostname));
+    char* user = get_username();
 
+    printf("%s@%s>", user, host);
 	
 	while(!close) {
-		char* input = readline("> ");
 		
 	}
 	return 0;
